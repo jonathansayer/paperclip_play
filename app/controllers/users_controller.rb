9dc629faaf2f7462a85d06d04251be4ae477357c
jonathansayer <jon.sayer1@gmail.com>
2015-11-10 18:53:39 +0000

    root changed to index and former create page now index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+  
   def create
+    p "hello"
+    p params[:user]
     @user = User.create( params[:user] )
+  end
+
+  def show
   end
 end
